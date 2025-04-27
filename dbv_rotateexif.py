ebfe26f5a5e4f4d9731c6e4b935724b3f2638ba7
Martin Hildebrandt <martin@wak-lab.org>
2025-04-27 22:27:40 +0200

    relative Drehung repariert2

--- dbv_rotateexif.py
+++ dbv_rotateexif.py
@@ -27,19 +27,19 @@ def rotate_exif(image_path, output_path, angle):
     current_orientation = get_current_orientation(exif_dict)
 
     # Exif-Daten anpassen (Orientierung)
-    if angle is not None:
+    if (angle is None) or (angle == 0):
         # Absoluter Winkel
-        new_orientation = angle
+        new_orientation = current_orientation
     else:
         # Relative Drehung
-        if current_orientation == 1:  # Keine Drehung
-            new_orientation = 1
+        if current_orientation == 1:  # Keine Drehung 1 6 3 8
+            new_orientation = 6 if angle == 90 else (3 if angle == 180 else 8)
         elif current_orientation == 6:  # 90 Grad
-            new_orientation = 3 if angle == 180 else (8 if angle == 270 else 6)
+            new_orientation = 3 if angle == 90 else (8 if angle == 180 else 1)
         elif current_orientation == 3:  # 180 Grad
-            new_orientation = 6 if angle == 90 else (1 if angle == 270 else 3)
+            new_orientation = 8 if angle == 90 else (1 if angle == 180 else 6)
         elif current_orientation == 8:  # 270 Grad
-            new_orientation = 1 if angle == 90 else (6 if angle == 270 else 3)
+            new_orientation = 1 if angle == 90 else (6 if angle == 180 else 3)
 
     exif_dict['0th'][274] = new_orientation  # Setze die neue Orientierung
 
@@ -55,7 +55,7 @@ def main():
     parser = argparse.ArgumentParser(description='Ändere die Exif-Orientierung eines JPG-Bildes.')
     parser.add_argument('image_file', type=str, help='Der Pfad zur JPG-Datei.')
     parser.add_argument('output_image', type=str, help='Der Pfad zur Ausgabedatei (JPG).')
-    parser.add_argument('angle', type=int, choices=[0, 90, 180, 270], help='Absolut 0, 90, 180, 270')
+    parser.add_argument('angle', type=int, choices=[0, 90, 180, 270], help='Der Drehwinkel (0, 90, 180, 270 Grad).')
 
     args = parser.parse_args()
 
@@ -71,4 +71,5 @@ def main():
     exit(status)
 
 if __name__ == '__main__':
+    #rotate_exif("../source/h.jpg", "../source/hr.jpg", 90)
     main()
