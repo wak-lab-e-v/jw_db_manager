133aad799c8a23c466be5b04f67b7bcc4e85b090
User <user@example.com>
2025-04-27 19:17:01 +0200

    bind flask on all interfaces

--- db_viewer_web.py
+++ db_viewer_web.py
@@ -520,4 +520,4 @@ def serve_image(entry_id, filename):
     return Response(image_data, mimetype=content_type)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
